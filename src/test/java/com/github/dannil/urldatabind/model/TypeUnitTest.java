e4fe90e4dc6e7bf72cc267535cd3b0112d03c30c
Daniel Nilsson <daniel.nilsson94@outlook.com>
2015-12-23 22:27:02 +0100

    Added unit test for Type

---
+++ src/test/java/com/github/dannil/urldatabind/model/TypeUnitTest.java
@@ -0,0 +1,27 @@
+package com.github.dannil.urldatabind.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class TypeUnitTest {
+
+	@Test
+	public void valueOf() {
+		Type type = Type.valueOf("JSON");
+
+		assertEquals(Type.JSON, type);
+	}
+
+	@Test
+	public void values() {
+		Type[] types = Type.values();
+
+		assertNotNull(types);
+	}
+
+}
