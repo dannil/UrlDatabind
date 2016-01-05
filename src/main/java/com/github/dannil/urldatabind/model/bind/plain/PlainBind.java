a58d334c4f9ea135e84d8ddc96252f44d5d641e0
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-01-05 23:01:03 +0100

    Added PlainBind (text)

---
+++ src/main/java/com/github/dannil/urldatabind/model/bind/plain/PlainBind.java
@@ -0,0 +1,23 @@
+package com.github.dannil.urldatabind.model.bind.plain;
+
+import com.github.dannil.urldatabind.model.RequestMethod;
+import com.github.dannil.urldatabind.model.Type;
+import com.github.dannil.urldatabind.model.bind.Bind;
+
+public class PlainBind<E> extends Bind<E> {
+
+	protected PlainBind(String path, RequestMethod requestMethod, E content) {
+		super(path, requestMethod, content);
+	}
+
+	@Override
+	public Type getType() {
+		return Type.PLAIN;
+	}
+
+	@Override
+	public String getHttpType() {
+		return "text/plain";
+	}
+
+}
