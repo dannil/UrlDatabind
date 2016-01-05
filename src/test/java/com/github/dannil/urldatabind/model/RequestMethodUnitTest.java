1f2ed66b8da3953f1b2d1c130919694febd21cb0
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-01-05 23:12:26 +0100

    Added RequestMethod unit test

---
+++ src/test/java/com/github/dannil/urldatabind/model/RequestMethodUnitTest.java
@@ -0,0 +1,24 @@
+package com.github.dannil.urldatabind.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class RequestMethodUnitTest {
+
+	@Test
+	public void valueOf() {
+		RequestMethod method = RequestMethod.valueOf("GET");
+
+		assertEquals(RequestMethod.GET, method);
+	}
+
+	@Test
+	public void values() {
+		RequestMethod[] methods = RequestMethod.values();
+
+		assertNotNull(methods);
+	}
+
+}
