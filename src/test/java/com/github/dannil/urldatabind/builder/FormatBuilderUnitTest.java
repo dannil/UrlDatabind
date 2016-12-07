83d34ce242c535d24281c21df8eca4eff55f2bf1
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-12-07 18:45:56 +0100

    Added test

--- src/test/java/com/github/dannil/urldatabind/builder/FormatBuilderUnitTest.java
+++ src/test/java/com/github/dannil/urldatabind/builder/FormatBuilderUnitTest.java
@@ -1,8 +1,12 @@
 package com.github.dannil.urldatabind.builder;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Locale;
 
 import org.junit.Test;
@@ -41,4 +45,36 @@ public class FormatBuilderUnitTest {
 
 		formatBuilder.getHttpFormat(null, jsonBind);
 	}
+
+	@Test
+	public void callPrivateConstructorSingletonHolder() throws NoSuchMethodException, SecurityException,
+			InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+
+		// Ridiculous code to instantiate private static class inside a private
+		// class
+		Class<?> enclosingClass = FormatBuilder.class;
+		Constructor<?> enclosingConstructor = enclosingClass.getDeclaredConstructor();
+		enclosingConstructor.setAccessible(true);
+
+		// Make sure we instantiate the correct inner class
+		String innerClassName = "SingletonHolder";
+		Class<?> innerClass = null;
+		Class<?>[] innerClasses = FormatBuilder.class.getDeclaredClasses();
+		for (int i = 0; i < innerClasses.length; i++) {
+			if (innerClasses[i].toString().contains(innerClassName)) {
+				// We found the correct class!
+				innerClass = innerClasses[i];
+			}
+		}
+		assertTrue(innerClass.toString().contains(innerClassName));
+
+		Constructor<?> innerConstructor = innerClass.getDeclaredConstructors()[0];
+
+		innerConstructor.setAccessible(true);
+		innerConstructor.newInstance();
+		innerConstructor.setAccessible(false);
+
+		assertFalse(innerConstructor.isAccessible());
+	}
+
 }
