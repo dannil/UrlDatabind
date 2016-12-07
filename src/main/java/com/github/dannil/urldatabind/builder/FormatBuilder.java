a223821bd951c9d9c59245487f418ffdbaa9a6bc
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-12-07 17:56:24 +0100

    Changed wrong visibility modifier

--- src/main/java/com/github/dannil/urldatabind/builder/FormatBuilder.java
+++ src/main/java/com/github/dannil/urldatabind/builder/FormatBuilder.java
@@ -61,7 +61,7 @@ public final class FormatBuilder {
 		/**
 		 * Private constructor.
 		 */
-		public SingletonHolder() {
+		private SingletonHolder() {
 			// To avoid initialization outside this class
 		}
 
