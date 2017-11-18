035e4eed8dafa466a6feb663957af505720464ae
Daniel Nilsson <daniel.nilsson94@outlook.com>
2017-11-18 16:56:34 +0100

    Broke out HTTP code to separate branch

--- src/main/java/com/github/dannil/urldatabind/Main.java
+++ src/main/java/com/github/dannil/urldatabind/Main.java
@@ -1,12 +1,8 @@
 package com.github.dannil.urldatabind;
 
 import java.time.ZonedDateTime;
-import java.util.HashSet;
-import java.util.Set;
 
-import com.github.dannil.urldatabind.http.BindHttpServer;
 import com.github.dannil.urldatabind.model.RequestMethod;
-import com.github.dannil.urldatabind.model.bind.AbstractBind;
 import com.github.dannil.urldatabind.model.bind.JsonBind;
 import com.github.dannil.urldatabind.model.bind.XmlBind;
 import com.github.dannil.urldatabind.model.bind.result.ObjectResult;
@@ -25,13 +21,6 @@ public class Main {
 
         JsonBind jsonBind2 = new JsonBind("person/daniel/json", RequestMethod.GET, objectResult);
         XmlBind xmlBind = new XmlBind("person/daniel/xml", RequestMethod.GET, objectResult);
-
-        Set<AbstractBind> binds = new HashSet<AbstractBind>();
-        binds.add(jsonBind2);
-        binds.add(xmlBind);
-
-        BindHttpServer server = new BindHttpServer(binds);
-        server.start(5433);
     }
 
 }
